e95cd6364deeeabb06d3810d811a137b60ce4841
Victor Gunnarsson <victor@educar.se>
2020-05-18 09:27:32 +0200

    Cleaned python code

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ app = Flask(__name__)
 CORS(app)
 app.config['JSON_AS_ASCII'] = False
 
-@app.route('/') # simple index method
-def index():
-    return "Edaa35 test application"
-
-@app.route('/api/test') # returnning a simple json
-def getTestjson():
-    return jsonify({'Test': 'testing'})
-
 @app.route('/api/sort', methods=['POST']) 
 def flaskLogin():
     if not request.json or not 'UnsortedList' in request.json:
@@ -22,10 +14,6 @@ def flaskLogin():
         array = request.json['UnsortedList']
         sort.quickSort(array, 0, len(request.json['UnsortedList']) - 1)
         return jsonify({'sortedList': array}) 
-
-@app.errorhandler(404) # error handler to answer 404 with json
-def notFound(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
